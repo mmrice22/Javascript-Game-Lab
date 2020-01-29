9db1ebb6467bea6e0980d712f7ada70dc3540fe3
Melissa Rice <mmrice22@gmail.com>
2020-01-29 13:52:00 -0500

    randNum declared

---
+++ script.js
@@ -0,0 +1,8 @@
+"use sctipt"
+const randomDamage = () => console.log(Math.floor(Math.random() * 10) + 1);
+// to show that function above works
+// console.log(randomDamage());
+
+const randNum = (opt1, opt2) => console.log(Math.round(Math.random()));
+// to show that the function above works
+// console.log(randNum());
