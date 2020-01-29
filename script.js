9f541754e393abd7f53e2971426e9c0f9aac9186
Melissa Rice <mmrice22@gmail.com>
2020-01-29 14:49:58 -0500

    at isDead ternary

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-"use sctipt"
-const randomDamage = () => console.log(Math.floor(Math.random() * 10) + 1);
+"use sctipt";
+const randomDamage = () => Math.floor(Math.random() * 10) + 1;;
 // to show that function above works
 // console.log(randomDamage());
 
-const randNum = (opt1, opt2) => console.log(Math.round(Math.random()));
-// to show that the function above works
-// console.log(randNum());
+
+const chooseOption = (opt1, opt2) => {
+    let randNum = Math.floor(Math.random()*2);
+    // ternary operator - if randNum is 0, run option 1. if its not, run opt 2
+    return randNum === 0 ? opt1 : opt2;
+}
+// to show that the above function works
+// console.log(chooseOption("Mitch","melissa"))
+
+// FUNCTION EXPRESSION
+const attackPlayer = function (health) {
+    return health - randomDamage;
+}
+
+// Arrow function Health
+const logHealth = (player, health) => {
+ console.log(`${player} health: ${health}.`);
+}
+
+// Arrow function Winner/Loser
+const logDeath = (winner, loser) => {
+    console.log(`${winner} defeated ${loser}`);
+}
+
+// Arrow Function Dead
+const isDead = (health) => {
+    return health <= 0 ? true : false;
+}
